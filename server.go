202de08224e94fa0ebebdf7b42c98412ed2d7199
43h <cw.clx@outlook.com>
2024-11-24 16:22:00 +0800

    Update server.go

--- server.go
+++ server.go
@@ -1,6 +1,3 @@
-//go:build linux
-// +build linux
-
 package main
 
 import (
@@ -19,19 +16,21 @@ var listener net.Listener
 func initServer() bool {
 	tmpListener, err := net.Listen("tcp", ConfigParam.Listen)
 	if err != nil {
-		fmt.Println("Error listening:", err)
+		LOGE("Error listening:", err)
 		return false
 	}
 
 	file, err := tmpListener.(*net.TCPListener).File()
 	if err != nil {
-		fmt.Println("Error getting file descriptor:", err)
+		LOGE("Error getting file descriptor:", err)
+		return false
 	}
 	fd := int(file.Fd())
 
 	err = syscall.SetsockoptInt(fd, syscall.SOL_IP, syscall.IP_TRANSPARENT, 1)
 	if err != nil {
-		fmt.Println("Error setting IP_TRANSPARENT:", err)
+		LOGE("Error setting IP_TRANSPARENT:", err)
+		return false
 	}
 	listener = tmpListener
 	return true
@@ -41,24 +40,22 @@ func closeServer() {
 	if listener != nil {
 		err := listener.Close()
 		if err != nil {
-			fmt.Println("Error closing listener:", err)
+			LOGE("Error closing listener:", err)
 		} else {
-			fmt.Println("Server closed")
+			LOGI("Server closed")
 		}
-		fmt.Println("Error closing listener:", err)
 	} else {
-		fmt.Println("Server closed")
+		LOGI("Server closed(skip)")
 	}
 }
 
 func startServer() {
-	fmt.Println("Server started")
-	fmt.Println("Listening on ", ConfigParam.Listen)
+	LOGI("Server started Listening on ", ConfigParam.Listen)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting:", err)
+			LOGE("Error accepting:", err)
 			continue
 		}
 
@@ -79,55 +76,50 @@ func getOriginalDst(conn *net.TCPConn) (string, error) {
 	} else {
 		switch addr := sa.(type) {
 		case *unix.SockaddrInet4:
-			// 打印 IPv4 地址
 			ip := net.IP(addr.Addr[:]).String()
 			port := addr.Port
-			fmt.Printf("rcv connect from IPv4 Address: %s, Port: %d\n", ip, port)
+			LOGI("rcv connect from IPv4 Address: %s, Port: %d\n", ip, port)
 			return ip + ":" + strconv.Itoa(port), nil
 		//case *unix.SockaddrInet6:
-		//	// 打印 IPv6 地址
 		//	ip := net.IP(addr.Addr[:]).String()
 		//	port := addr.Port
 		//	fmt.Printf("IPv6 Address: %s, Port: %d\n", ip, port)
 
 		//case *unix.SockaddrUnix:
-		//	// 打印 Unix 域套接字地址
 		//	fmt.Printf("Unix Socket Path: %s\n", addr.Name)
 
 		default:
-			fmt.Println("Unknown address type")
 		}
 	}
 	return "", errors.New("Unknown address type")
 }
 
 func handleRequest(conn net.Conn) {
-	// 处理连接的逻辑
-	fmt.Println("New connection accepted")
+	LOGI("New connection accepted")
 	ipstr, err := getOriginalDst(conn.(*net.TCPConn))
 	if err != nil || ipstr == "" {
 		err := conn.Close()
 		if err != nil {
-			fmt.Println("Error closing connection:", err)
+			LOGE("Error closing new connection:", err)
 			return
 		}
 	}
 	connID := uuid.New().String()
-	fmt.Printf("Connection ID: %s\n", connID)
+	LOGI("new Connection ID: %s\n", connID)
 
 	clientAddEventConnect(connID, ipstr, conn)
-	// 读取客户端发送的数据
-	buf := make([]byte, 2048)
+
+	buf := make([]byte, 10240)
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println("Error reading:", err)
+			LOGE("Error reading, ", connID, " ", err)
 			conn.Close()
 			clientAddEventDisconnect(connID)
 			return
 		} else {
-			clientAddEventMsg(connID, buf, n)
+			LOGI("Read from client:", connID, " length:", n)
+			clientAddEventMsg(connID, buf[:n], n)
 		}
 	}
-
 }
