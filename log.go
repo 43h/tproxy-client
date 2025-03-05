b5871c4609c31759f71acff2957d1045a9415197
43h <cw.clx@outlook.com>
2025-03-05 22:22:40 +0800

    区分日志打印

--- log.go
+++ log.go
@@ -19,22 +19,22 @@ var logLevel = INFO
 var logHandle *os.File
 
 func initLog(debug bool) bool {
-	if debug == true {
+	delLog()
+
+	if debug == true { //debug mode
 		logLevel = DEBUG
 		return true
 	}
 
-	delLog()
 	fileName := time.Now().Format("2006-01-02-15-04-05") + ".log"
 	logHandle, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Println("fail to open file, ", err)
 		return false
+	} else {
+		log.SetOutput(logHandle)
+		log.SetFlags(log.Lshortfile | log.LstdFlags)
 	}
-
-	log.SetOutput(logHandle)
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
-
 	return true
 }
 
@@ -66,23 +66,5 @@ func delLog() {
 
 	if err != nil {
 		LOGE("fail to del log, ", err)
-	}
-}
-
-func LOGD(v ...interface{}) {
-	if logLevel <= DEBUG {
-		log.Println("[ DEBUG] ", v)
-	}
-}
-
-func LOGI(v ...interface{}) {
-	if logLevel <= INFO {
-		log.Println("[ INFO] ", v)
-	}
-}
-
-func LOGE(v ...interface{}) {
-	if logLevel <= ERROR {
-		log.Println("[ERROR] ", v)
 	}
 }
